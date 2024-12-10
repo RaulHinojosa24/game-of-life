fe10a414731ce5ed8b19931c5a7d143e8f7a4f97
Raul Hinojosa Perez <raul.hinojosa.perez@gmail.com>
2024-12-10 14:36:31 +0100

    Pencil icon fix?

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-new */
 import P5 from 'p5'
+import PencilIcon from './assets/pencil.ico'
 
 const main = document.querySelector('main')
 const boardCanvas = document.querySelector('#boardCanvas')
@@ -104,7 +105,7 @@ const sketch = (p5) => {
       drawingMode = cleanValue
       drawInput.checked = cleanValue
 
-      boardCanvas.style.cursor = cleanValue ? 'url(\'/src/assets/pencil.ico\'), pointer' : 'auto'
+      boardCanvas.style.cursor = cleanValue ? `url(${PencilIcon}), pointer` : 'auto'
     }
   }
 
